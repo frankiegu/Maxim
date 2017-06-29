29a67c93b7f358e4defa967731e8f018efc5a28c
Yami Odymel <yamiodymel@gmail.com>
2017-06-29 16:36:25 +0800

    x

---
+++ context.go
@@ -0,0 +1,16 @@
+package maxim
+
+type Context struct {
+}
+
+func (c *Context) Bind() {
+
+}
+
+func (c *Context) Respond() {
+
+}
+
+func (c *Context) RespondOthers() {
+
+}
--- test/script.src.js
+++
@@ -1,7 +0,0 @@
-/*document.getElementById("#button").addEventListener("click", async() => {
-    var conn, result
-    conn = new Maxim("ws://localhost/")
-    result = await conn.execute("HelloWorld", {
-        hello: ", world!"
-    })
-})*/
